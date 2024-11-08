db92da55d103442429b696c87f715a78a1ca2e0d
Setare <setare.hajibagheri@gmail.com>
2024-11-08 15:30:17 +0330

    create current time

---
+++ src/Component/CurrentTime/CurrentTime.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import { Box, Typography } from '@mui/material'
+import AccessAlarmOutlinedIcon from '@mui/icons-material/AccessAlarmOutlined';
+import dayjs from 'dayjs';
+
+
+type Props = {}
+
+export default function CurrentTime({}: Props) {
+  const [currentTime, setCurrentTime] = useState(dayjs);
+
+  useEffect(() => {
+    const timer = setInterval(() => { setCurrentTime(dayjs()); }, 1000);
+    return () => clearInterval(timer);
+  },[])
+
+  return (
+    <Box sx={{
+      display:'flex',
+      flexDirection:'row',
+      alignItems: 'center',
+      justifyContent: 'right'
+      }}>
+      <AccessAlarmOutlinedIcon 
+        sx={{mr: 1}} 
+        fontSize='large' 
+      />
+      <Typography>
+        {
+          currentTime.format('YYYY-MM-DD - HH:mm:ss')
+        }
+      </Typography>
+    </Box>
+  )
+}
